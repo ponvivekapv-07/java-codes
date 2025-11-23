dcffe13ad20b8d2f3669e0d669848d7054a2c750
Ponviveka M <vivekaviveka2004@gmail.com>
2025-11-23 18:54:56 +0530

    MathsOperations.java added

---
+++ MathsOperations.java
@@ -0,0 +1,12 @@
+class MathsOperations{
+	public static void main(String[]args)
+	{
+		//Declaring the variables
+		int firstNum=25;
+		int secondNum=5;
+		System.out.println(firstNum+secondNum); //adding the given variables
+		System.out.println(firstNum-secondNum); //subtracting the variable from another one
+		System.out.println(firstNum*secondNum); //multiplying the given variables
+		System.out.println(firstNum/secondNum); //diving the variable from the another one
+	}
+}
