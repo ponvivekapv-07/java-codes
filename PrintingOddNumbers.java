470f1c1a6a3c8ee7db85d65dd94df43d28414365
Ponviveka M <vivekaviveka2004@gmail.com>
2025-11-23 19:25:24 +0530

    Add program to print odd numbers in a range

---
+++ PrintingOddNumbers.java
@@ -0,0 +1,10 @@
+class PrintingOddNumbers {
+  public static void main(String[] args) {
+  System.out.println("The odd numbers between the given range:");
+  for(int i=1;i<=20;i++){
+  	if(i % 2 != 0){  //checking the number whether it is odd or even
+    System.out.println(i);
+    }
+    }  
+  }
+}
